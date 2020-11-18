a1743176c0d8b631c2d849ce61b9594ab069ad29
Douglas <douglaza@gmail.com>
2020-11-18 18:59:05 -0300

    requirement 01

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
-
 const app = express();
-
+const middlewares = require('./middlewares');
 const PORT = 3000;
-
-app.listen(PORT, () => console.log('listening on 3k'));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+// console.log(`chave: ${token}`);
+
+app.use(express.json());
+
+// app.use(middlewares.auth);
+
+app.post('/login', middlewares.auth);
+
+app.get('/ping', (_, res) => {
+  res.json({ message: 'ping test' });
+});
+
+app.listen(PORT, () => console.log('listening on 3k'));
---
+++ middlewares/index.js
@@ -0,0 +1,9 @@
+// const error = require('./error');
+// const logger = require("./logger");
+const auth = require('./auth');
+
+module.exports = {
+  // error,
+  // logger,
+  auth,
+};
