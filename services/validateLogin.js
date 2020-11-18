eee0e99b003efdf52b7a27177a2e2365e0ed85ac
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-11-18 18:06:18 -0300

    using services and checking ifs token

---
+++ services/validateLogin.js
@@ -0,0 +1,15 @@
+function validateEmail(email) {
+  const re = /^[^@]+@[^@]+\.[^@]+$/;
+  return re.test(String(email).toLowerCase());
+} // returns true or false
+// ref regex https://stackoverflow.com/questions/742451/what-is-the-simplest-regular-expression-to-validate-emails-to-not-accept-them-bl
+
+function validatePassword(password) {
+  if (password.length > 5) {
+    return true
+  } else {
+    return false
+  };
+}
+
+module.exports = { validateEmail, validatePassword };
