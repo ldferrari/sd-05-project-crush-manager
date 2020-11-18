ec316597fbad08eaed16fdf7e01807ac876ca908
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-11-18 16:08:48 -0300

    good practice for middleware organization and bodyParser

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 
 const app = express();
+// app.use(express.json()); melhor pratica de usar bodyParser
 
 // Importações
+const bodyParser = require('body-parser');
+const middlewares = require('./middlewares');
 
 // Port listening
 const PORT = 3000;
 app.listen(PORT, () => console.log('3000 port OK!'));
 
 // Middlewares diversos
+app.use(bodyParser.json());
+// app.use(middlewares.namemw);
 
 // ENDPOINTS
 
@@ -20,3 +25,4 @@ app.get('/', (request, response) => {
 
 
 // Middlewares de erro
+// app.use(middlewares.error);
---
+++ middlewares/index.js
@@ -0,0 +1,9 @@
+// const logger = require('logger');
+// const error = require('error');
+// const mw = requires('error');
+
+// module.exports = {
+//   logger,
+//   error,
+//   mw
+// }
