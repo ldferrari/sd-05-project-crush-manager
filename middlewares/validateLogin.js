501f5c6715f2c83718ba3e59dcd27f10092fa1d8
Felipe Vieira <flpvieira86@gmail.com>
2020-11-18 17:39:30 -0300

    validacao se email esta correto

--- middlewares/validateLogin.js
+++ middlewares/validateLogin.js
@@ -2,11 +2,18 @@ const rescue = require('express-rescue');
 
 const validateLogin = rescue(async (req, res, next) => {
   const { email } = req.body;
+  const regex = /^[a-zA-Z0-9._]+@[a-zA-Z0-9]+\.[A-Za-z]+$/;
 
   if (!email) {
     return res.status(400).json({
       message: 'O campo "email" é obrigatório',
     });
+  }
+
+  if (!regex.test(email)) {
+    return res
+      .status(400)
+      .json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
 
   return next();
