25953c77fb472855df9af95c0c70085f41c9cd0c
Felipe Vieira <flpvieira86@gmail.com>
2020-11-18 17:42:24 -0300

    validacao se password existe

--- middlewares/validateLogin.js
+++ middlewares/validateLogin.js
@@ -1,19 +1,25 @@
 const rescue = require('express-rescue');
 
 const validateLogin = rescue(async (req, res, next) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
   const regex = /^[a-zA-Z0-9._]+@[a-zA-Z0-9]+\.[A-Za-z]+$/;
 
   if (!email) {
-    return res.status(400).json({
-      message: 'O campo "email" é obrigatório',
-    });
+    return res
+      .status(400)
+      .json({ message: 'O campo "email" é obrigatório' });
   }
 
   if (!regex.test(email)) {
     return res
       .status(400)
       .json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+
+  if (!password) {
+    return res
+      .status(400)
+      .json({ message: 'O campo "password" é obrigatório' });
   }
 
   return next();
