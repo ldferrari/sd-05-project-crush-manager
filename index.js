24183f6abdf9c8e851eaef9ab485650f00cc3025
Douglas <douglaza@gmail.com>
2020-11-18 15:59:47 -0300

    listen

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+const PORT = 3000;
+
+app.listen(PORT, () => console.log('listening on 3k'));
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
