39f679c58e3f557090ddbb1ab5aaadfd022a7a44
Felipe Vieira <flpvieira86@gmail.com>
2020-11-18 20:26:54 -0300

    validaçao idade do crush

--- middlewares/validateCrush.js
+++ middlewares/validateCrush.js
@@ -1,18 +1,30 @@
 const rescue = require('express-rescue');
 
 const validateCrush = rescue(async (req, res, next) => {
-  const { name } = req.body;
+  const { name, age } = req.body;
 
   if (!name) {
-    return res.status(400).json({
-      message: 'O campo "name" é obrigatório',
-    });
+    return res
+      .status(400)
+      .json({ message: 'O campo "name" é obrigatório' });
   }
 
   if (name.length < 3) {
-    return res.status(400).json({
-      message: 'O "name" deve ter pelo menos 3 caracteres',
-    });
+    return res
+      .status(400)
+      .json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+
+  if (!age) {
+    return res
+      .status(400)
+      .json({ message: 'O campo "age" é obrigatório' });
+  }
+
+  if (age < 18) {
+    return res
+      .status(400)
+      .json({ message: 'O crush deve ser maior de idade' });
   }
 
   next();
