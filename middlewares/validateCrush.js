fd3beac6384401f97f64e5c5bc425c5f4e821c7d
Felipe Vieira <flpvieira86@gmail.com>
2020-11-18 20:21:06 -0300

    validaçao tamanho do nome do crush

--- middlewares/validateCrush.js
+++ middlewares/validateCrush.js
@@ -9,6 +9,12 @@ const validateCrush = rescue(async (req, res, next) => {
     });
   }
 
+  if (name.length < 3) {
+    return res.status(400).json({
+      message: 'O "name" deve ter pelo menos 3 caracteres',
+    });
+  }
+
   next();
 });
 
