65591a5bf757ac7e392db726235094717e9ab1c3
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-11-18 19:21:45 -0300

    mid - req 2

--- index.js
+++ index.js
@@ -48,8 +48,43 @@ app.post('/login', (req, res) => {
 });
 
 // 2 - Crie o endpoint POST /crush
-// app.post('/crush', (req, res) => {
-// }
+app.post('/crush', (req, res) => {
+  const { name, age, date } = req.body;
+  //ifs do name
+  if (!name) {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  //ifs do age
+  if (!age) {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (age < 18) {
+    return res.status(400).json({ message: 'O crush deve ser maior de idade'});
+  }
+  //ifs do date
+  if (date.rate < 1 || date.rate > 5 || !(Number.isInteger(date.rate)) ) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5'});
+  }
+  if (!checkDatedAt(date.datedAt)) {
+    return res.status(400).json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"'});
+  }
+  if (!date.datedAt || !date.rate) {
+    return res.status(400).json({  message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios'});
+  }
+  // token
+  
+});
+
+function checkDatedAt(data) {
+  const re = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/;
+  return re.test(String(data));
+}
+// pacote moment é outra opçao
+
+
 
 // Middlewares de erro
 // app.use(middlewares.error);
