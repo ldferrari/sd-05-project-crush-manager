18b26330638c03352175a4b69b7dee331bb0a299
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-11-18 19:18:19 -0200

    Fixing test

--- dictionary/errors.dictionary.js
+++ dictionary/errors.dictionary.js
@@ -2,5 +2,5 @@ module.exports = {
   NO_EMAIL: 'O campo "email" é obrigatório',
   NO_PASS: 'O campo "password" é obrigatório',
   INVALID_EMAIL: 'O "email" deve ter o formato "email@email.com"',
-  INVALID_PASS: 'A "senha" ter pelo menos 6 caracteres',
+  INVALID_PASS: 'A "senha" deve ter pelo menos 6 caracteres',
 };
