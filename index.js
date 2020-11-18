500c4a132ef09797c95d5204bd1851fbbb1d4e38
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-11-18 14:11:14 -0300

    npm install and basic structure

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ const express = require('express');
 
 const app = express();
 
-// não remova esse endpoint, e para o avaliador funcionar
+// Importações
+
+// Port listening
+const PORT = 3000;
+app.listen(PORT, () => console.log('3000 port OK!'));
+
+// Middlewares diversos
+
+// ENDPOINTS
+
+// 0 - não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+
+
+// Middlewares de erro
