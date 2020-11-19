4e10bef8a54328c0ef70416994cb0dde3398fc88
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-11-19 14:32:50 -0300

    new try for req2

--- middlewares/addCrush.js
+++ middlewares/addCrush.js
@@ -1,10 +1,11 @@
 const { readCrushFile, writeCrushFile } = require('../services/addCrushFunctions');
 
 module.exports = async (req, res) => {
+  console.log("aqui no addCrush");
   const { name, age, date } = req.body;
   const currentCrushList = await readCrushFile();
   const id = currentCrushList.length + 1;
   writeCrushFile(currentCrushList, { id, name, age, date });
   res.status(201).json({ id, name, age, date });
-  // console.log(`New crush should appear: ${req.body}`);
+  // bem adicionado no arquivo crush.json
 };
--- services/addCrushFunctions.js
+++ services/addCrushFunctions.js
@@ -1,14 +1,15 @@
 const fs = require('fs').promises;
 
 const readCrushFile = async () => {
-  const crushList = await fs.readFile('../crush.json', 'utf-8');
+  const crushList = await fs.readFile('./crush.json', 'utf-8');
+  // parecia ../crush.json, mas function chamada em outro lugar modifica path
   return JSON.parse(crushList);
 };
 
 const writeCrushFile = async (currentCrushList, newCrush) => {
   const newCrushList = [...currentCrushList, newCrush];
   // const newCrushList = currentCrushList.push(newCrush);
-  fs.writeFile('../crush.json', JSON.stringify(newCrushList));
+  fs.writeFile('./crush.json', JSON.stringify(newCrushList));
 };
 
 module.exports = { readCrushFile, writeCrushFile };
