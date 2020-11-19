0d34931819de6578076f5e95ec4e8ac8bb15c87c
Mau <mauricio.sdjr@gmail.com>
2020-11-19 14:44:31 -0300

    requisito 1

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const middlewares = require('./middlewares');
 
 const app = express();
+const PORT = 3000;
+const token = crypto.randomBytes(16).toString('hex');
+
+app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.post('/login', middlewares.auth, (_req, res, _next) => {
+  res.status(200).json({ token });
+});
+
+app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
