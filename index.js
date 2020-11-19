bb75496d77f0a2926c92933b0fa944b8156a862b
Rafael Assad <rafaassadeng@gmail.com>
2020-11-19 15:55:22 -0300

    1

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const randToken = require('rand-token');
+const middleware = require('./middlewares');
 
 const app = express();
+app.use(express.json());
+const generateToken = randToken.generate(16);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
+});
+
+app.post('/login', middleware.email, middleware.password, (req, res, _next) => {
+    const newtoken = generateToken 
+    res.json({ token: newtoken})
 });
 
 app.listen(3000, () => {
---
+++ middlewares/email.js
@@ -0,0 +1,16 @@
+const regex = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
+
+module.exports = (req, res, next) => {
+  if (req.body.email === undefined) {
+    return res.status(400).send({
+      message: 'O campo "email" é obrigatório',
+    });
+  }
+  if (req.body.email.match(regex)) {
+    return next();
+  }
+
+  res.status(400).send({
+    message: 'O "email" deve ter o formato "email@email.com"',
+  });
+};
