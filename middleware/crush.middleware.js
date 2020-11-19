72cd655b22c113f8bf264c1d5b8aebe2564c8017
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-11-19 14:46:45 -0200

    Fixed

--- middleware/crush.middleware.js
+++ middleware/crush.middleware.js
@@ -14,8 +14,8 @@ const isValidName = ({ name }) => name.length >= 3;
 const isValidAge = ({ age }) => Number(age) >= 18;
 const isValidDate = ({ date }) => {
   if (typeof date !== 'object') return NO_DATE;
-  if (!date.datedAt) return NO_DATE; 
-  if (!date.rate) return NO_DATE
+  if (!date.datedAt) return NO_DATE;
+  if (!date.rate) return NO_DATE;
   return null;
 };
 const isValidDatedAt = ({ date: { datedAt } }) => datedAt.match(/\d{2}\/\d{2}\/\d{4}/ig)
