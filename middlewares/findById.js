ce30e150109bbfbe8e9700723ca0fd79e7ec955c
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-11-19 15:33:43 -0300

    fixing Number id

--- middlewares/findById.js
+++ middlewares/findById.js
@@ -1,11 +1,11 @@
 const { readCrushFile } = require('../services/addCrushFunctions');
 
 module.exports = async (req, res) => {
-  const { id } = req.params;
+  const { id } = Number(req.params);
   const crushList = await readCrushFile();
-  const idCrush = crushList.find((crush) => crush.id === id);
-  if (!idCrush) {
+  const foundCrush = crushList.find((crush) => crush.id === id);
+  if (!foundCrush) {
     return res.status(404).json({ message: 'Crush não encontrado' });
   }
-  return res.status(200).json(idCrush);
+  return res.status(200).json(foundCrush);
 };
