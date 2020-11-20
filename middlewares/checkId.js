e89ea34d87e6529f4eba837b63160f98d94e695c
Mau <mauricio.sdjr@gmail.com>
2020-11-20 18:23:43 -0300

    requisito 4

---
+++ middlewares/checkId.js
@@ -0,0 +1,9 @@
+const helpers = require('../helpers');
+
+module.exports = async (req, res, next) => {
+  const { id } = req.params;
+  const crushList = await helpers.readFileCrush();
+  const isValid = crushList.some((crush) => crush.id !== parseInt(id, 10));
+  if (!isValid) return res.status(404).json({ message: 'Crush não encontrado' });
+  return next();
+};
