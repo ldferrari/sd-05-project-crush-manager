8ae95e33fec22d91f3df6806e09d738e9ed971ce
Christian Duessel <duessel@gmail.com>
2020-11-20 08:28:20 -0300

    req1 with rand-token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,5 @@
-const crypto = require('crypto');
+// const crypto = require('crypto');
+const randtoken = require('rand-token')
 
 function verifyEmail(email) {
   const eRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -25,13 +26,14 @@ const auth = (req, res) => {
   if (!Email) {
     return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   } if (!emailIsValid) {
-    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com' });
   } if (!password) {
     return res.status(400).json({ message: 'O campo "password" é obrigatório' });
   } if (!passwordIsValid) {
     return res.status(400).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
   } if (emailIsValid && passwordIsValid) {
-    const rtoken = crypto.randomBytes(16).toString('hex');
+    const rtoken = randtoken.generate(16)
+    // const rtoken = crypto.randomBytes(16).toString('hex');
     return res.status(200).json({ token: rtoken });
   } return res.status(401).json('deu ruim');
 };
