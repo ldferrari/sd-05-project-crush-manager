777cc2b6698ddba30c06da9c7ec74dae4d5cf42d
César Alves <alvescesar@hotmail.com>
2020-11-20 18:40:04 -0300

    lint

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ app.get('/', (request, response) => {
   response.send();
 });
 
-app.listen(3000, () => {console.log('Listening to port 3000')});
+app.listen(3000, () => {
+  console.log('Listening to port 3000');
+});
