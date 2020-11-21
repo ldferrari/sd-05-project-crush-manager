94bd0a6de400bb280c4bd7459e9398c1e2950b02
hleoc <informig@gmail.com>
2020-11-21 19:33:04 -0300

    Desafio 1 - corrigindo erros do eslint - parte 4

--- routeSignup.js
+++ routeSignup.js
@@ -14,16 +14,16 @@ function validateEmail(email) {
 
 router.post('/', async (req, res) => {
   if (!req.body.email) {
-    return res.status(400).json({ message: `O campo \'email\' é obrigatório` });
+    return res.status(400).json({ message: 'O campo \'email\' é obrigatório' });
   }
   if (!validateEmail(req.body.email)) {
-    return res.status(400).json({ message: `O \'email\' deve ter o formato \'email@email.com\'` });
+    return res.status(400).json({ message: 'O \'email\' deve ter o formato \'email@email.com\'' });
   }
   if (!req.body.password) {
-    return res.status(400).json({ message: `O campo \'password\' é obrigatório` });
+    return res.status(400).json({ message: 'O campo \'password\' é obrigatório' });
   }
   if (req.body.password.length < 6) {
-    return res.status(400).json({ message: `A \'senha\' deve ter pelo menos 6 caracteres` });
+    return res.status(400).json({ message: 'A \'senha\' deve ter pelo menos 6 caracteres' });
   }
 
   const token = generateToken();
