72a711ae3a38dbda1d4f65f45c1d72c6130c81e8
hleoc <informig@gmail.com>
2020-11-21 19:09:32 -0300

    Desafio 1

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
-
 const app = express();
+const routeSignup = require('./routeSignup');
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use('/login', routeSignup);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
+});
+
+// -----------------------------------------------
+
+app.listen(PORT, () => {
+  console.log('O pai tá ON no projeto');
 });
