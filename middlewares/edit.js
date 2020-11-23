d02ca76a7b630e63b20db6e10ff39b70c2a6131d
Douglas <douglaza@gmail.com>
2020-11-23 13:03:27 -0300

    "Update"

--- middlewares/edit.js
+++ middlewares/edit.js
@@ -1,13 +1,15 @@
+const fs = require('fs').promises;
+
 const { readList } = require('../services');
 
 module.exports = async (req, res) => {
-  const { name, age, date } = await req.body;
+  const { name, age, date } = req.body;
   const crushList = await readList();
   const id = +req.params.id;
   // const deleteCrush = crushList.filter((crush) => crush.id !== id);
   // const newList = [...deleteCrush, { id, name, age, date }];
   const foundCrush = crushList.find((crush) => crush.id === id);
   crushList[crushList.indexOf(foundCrush)] = { id, name, age, date };
-  fs.writeFile('./crush.json', JSON.stringify(newList));
-  return res.status(200).json(newList);
+  fs.writeFile('./crush.json', JSON.stringify(crushList));
+  return res.status(200).json({ id, name, age, date });
 };
