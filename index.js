5d670eafa2d57e8f11c416c1e5251cfc512c7ee1
marinabrcls <barcelos.marina@gmail.com>
2020-11-23 18:31:43 -0300

    refatorado para utilizar middlewares já no login (requisito 1)

--- index.js
+++ index.js
@@ -2,81 +2,17 @@ const express = require('express');
 
 const bodyParser = require('body-parser');
 
-const crypto = require('crypto');
+const loginMid = require('./middlewares/login');
 
 const app = express();
 
-/* REQUISITO 1 - Crie o endpoint POST /login
-- O endpoint deve ser capaz de retornar um token aleatório de 16 caracteres
-que deverá ser utilizado nas demais requisições.
-- O corpo da requisição deverá ter o seguinte formato:
-{ "email": "email@email.com", "password": 123456 }
-- 4 possibilidades de erro (status 400): e-mail nulo, e-mail inválido,
-senha vazia, senha inválida;
-*/
-
 app.use(bodyParser.json());
 
-const token = crypto.randomBytes(8).toString('hex');
-/* https://stackoverflow.com/questions/8855687/secure-random-token-in-node-js */
-
-app.post('/login', (req, res) => {
-  const { email, password } = req.body;
-
-  const validateEmail = (userEmail) => {
-    const regex = /^[a-zA-Z0-9._]+@[a-zA-Z0-9]+\.[A-Za-z]+$/;
-    return regex.test(String(userEmail).toLowerCase());
-  };
-
-  /* fonte do regex: grepper e https://regular-expressions.info/ */
-
-  const validatePassword = (userPassword) => {
-    if (userPassword.length >= 6) {
-      return true;
-    }
-    return false;
-  };
-
-  if (!email) {
-    return res
-      .status(400)
-      .json({ message: 'O campo "email" é obrigatório' });
-  }
-
-  if (!validateEmail(email)) {
-    return res
-      .status(400)
-      .json({ message: 'O "email" deve ter o formato "email@email.com"' });
-  }
-
-  if (!password) {
-    return res
-      .status(400)
-      .json({ message: 'O campo "password" é obrigatório' });
-  }
-
-  if (!validatePassword(password)) {
-    return res
-      .status(400)
-      .json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
-  }
-
-  res.status(200).json({ token });
-});
+/* REQUISITO 1 - Crie o endpoint POST /login */
+app.post('/login', loginMid);
 
 /* ouvindo na porta 3000 */
 app.listen(3000, () => console.log('A mãe tá on na porta 3000!'));
-
-/* Ou podemos encadear as requisições para evitar repetir o caminho
-app
-.route('/')
-.get(function (req, res) {
-  res.send('hello world get');
-})
-.post(function (req, res) {
-  res.send('hello world post');
-});
-*/
 
 // não remova esse endpoint, eh para o avaliador funcionar
 app.get('/', (request, response) => {
---
+++ middlewares/login.js
@@ -0,0 +1,55 @@
+/*
+- O endpoint deve ser capaz de retornar um token aleatório de 16 caracteres
+que deverá ser utilizado nas demais requisições.
+- O corpo da requisição deverá ter o seguinte formato:
+{ "email": "email@email.com", "password": 123456 }
+- 4 possibilidades de erro (status 400): e-mail nulo, e-mail inválido,
+senha vazia, senha inválida;
+*/
+const crypto = require('crypto');
+
+const loginMid = (req, res) => {
+  const token = crypto.randomBytes(8).toString('hex');
+  /* https://stackoverflow.com/questions/8855687/secure-random-token-in-node-js */
+
+  const { email, password } = req.body;
+
+  const regex = /^[a-zA-Z0-9._]+@[a-zA-Z0-9]+\.[A-Za-z]+$/;
+  /* fonte do regex: grepper e https://regular-expressions.info/ */
+
+  const validateEmail = (userEmail) =>
+    regex.test(String(userEmail).toLowerCase());
+
+  const validatePassword = (userPassword) => {
+    if (userPassword.length >= 6) {
+      return true;
+    }
+    return false;
+  };
+
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+
+  if (!validateEmail(email)) {
+    return res
+      .status(400)
+      .json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+
+  if (!password) {
+    return res
+      .status(400)
+      .json({ message: 'O campo "password" é obrigatório' });
+  }
+
+  if (!validatePassword(password)) {
+    return res
+      .status(400)
+      .json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
+  }
+
+  return res.status(200).json({ token });
+};
+
+module.exports = loginMid;
