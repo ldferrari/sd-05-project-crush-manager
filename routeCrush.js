3967964851f85a1c76be900f41d1985ca0599b47
hleoc <informig@gmail.com>
2020-11-23 00:26:21 -0300

    Desafio 2

--- routeCrush.js
+++ routeCrush.js
@@ -1,6 +1,7 @@
 const fs = require('fs').promises;
 const express = require('express');
 const path = require('path');
+const moment = require('moment');
 
 const router = express.Router();
 
@@ -8,6 +9,14 @@ const readCrushFile = async () => {
   const content = await fs.readFile(path.resolve(__dirname, '.', 'crush.json'));
   return JSON.parse(content.toString('utf-8'));
 };
+
+const writeCrushFile = async (content) => (
+  fs.writeFile(path.resolve(__dirname, '.', 'crush.json'),
+    JSON.stringify(content),
+    (err) => {
+      if (err) throw err;
+    })
+);
 
 router.get('/', async (_req, res) => {
   const crush = await readCrushFile();
@@ -19,6 +28,59 @@ router.get('/:id', async (req, res) => {
   const { id } = req.params;
   const filteredCharacter = crush.find((character) => character.id === id) || [];
   res.status(200).send(filteredCharacter);
+});
+
+router.post('/', async (req, res) => {
+  const { name, age, date } = req.body;
+  // const { datedAt, rate } = date;
+  const { authorization } = req.headers;
+  if (!name) {
+    res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+  if (name.length < 3) {
+    res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  if (!age) {
+    res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (Number(age) < 18) {
+    res.status(400).json({ message: 'O crush deve ser maior de idade' });
+  }
+  if (!date || !date.datedAt || !date.rate) {
+    res.status(400).json({
+      message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios',
+    });
+  }
+  if (!moment(date.datedAt, 'DD/MM/AAAA').isValid()) {
+    res.status(400).json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  if (Number(date.rate) < 1 || Number(date.rate) > 5) {
+    res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  if (!authorization) {
+    res.status(401).json({ message: 'Token não encontrado' });
+  }
+  if (authorization && authorization.length !== 16) {
+    res.status(401).json({ message: 'Token inválido' });
+  }
+
+  const oldCrush = await readCrushFile();
+  const id = oldCrush.length + 1;
+  const { datedAt, rate } = date;
+  const newArrayOfCrush = [
+    ...oldCrush,
+    {
+      id,
+      name,
+      age,
+      date: {
+        datedAt,
+        rate,
+      },
+    },
+  ];
+  await writeCrushFile(newArrayOfCrush);
+  res.status(201).json(newArrayOfCrush[id - 1]);
 });
 
 module.exports = router;
