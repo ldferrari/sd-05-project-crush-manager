7a80112b4042f41a340ed75209db02117c04ca36
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-11-25 20:14:16 -0300

    ajustado

--- index.js
+++ index.js
@@ -17,14 +17,10 @@ app.get('/', (request, response) => {
   response.send();
 });
 
-// desafio 1
-
 app.post('/login', loginMid.validateLoginMidware, async (_req, res, _) => {
   const token = crypto.randomBytes(8).toString('hex');
   res.send({ token });
 });
-
-// desafio 2
 
 app.post('/crush', createCrush.createCrush, async (req, res, _) => {
   const { body } = req;
@@ -36,7 +32,6 @@ app.post('/crush', createCrush.createCrush, async (req, res, _) => {
   res.status(201).send(req.body);
 });
 
-// desafio 3
 app.get('/crush', async (req, res, next) => {
   const { authorization } = req.headers;
   if (authorization === undefined) {
@@ -57,11 +52,6 @@ app.get('/crush', async (_req, res, _next) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('on');
-});
-
-// desafio 4
 app.get('/crush/:id', crushId.byId, async (req, res, _) => {
   const { id } = req.params;
   const crushs = await fs.readFile('./crush.json', 'utf8');
@@ -69,12 +59,9 @@ app.get('/crush/:id', crushId.byId, async (req, res, _) => {
   res.status(200).json(list[id - 1]);
 });
 
-// desafio 5
 app.put('/crush/:id', createCrush.createCrush, async (req, res, _next) => {
-  const { name, age, date: { datedAt, rate } } = req.body;
-  const id = parseInt(req.params.id, 10);
-  // radix parameter: base da string, 10 indica sistema númerico decimal.
-  // fonte: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/parseInt
+  const { name, age, date: {datedAt, rate} } = req.body;
+  const id = parseInt(req.params.id);
   const list = await fs.readFile('./crush.json', 'utf8');
   const crushs = JSON.parse(list);
   const i = id - 1;
@@ -84,7 +71,6 @@ app.put('/crush/:id', createCrush.createCrush, async (req, res, _next) => {
   res.status(200).json(crushs[i]);
 });
 
-// desafio 6
 app.delete('/crush/:id', dc.deleteCrush, async (req, res, _next) => {
   const { id } = req.params;
   const i = id - 1;
@@ -94,4 +80,8 @@ app.delete('/crush/:id', dc.deleteCrush, async (req, res, _next) => {
   const newList = JSON.stringify(crush);
   fs.writeFile('./crush.json', newList, 'utf8');
   res.status(200).send({ message: 'Crush deletado com sucesso' });
+});
+
+app.listen(3000, () => {
+  console.log('on');
 });
