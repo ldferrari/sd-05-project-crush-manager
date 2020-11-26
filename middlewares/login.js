2293dee80e719b3472583e98eab34eb942b91bd3
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-11-26 02:26:18 -0300

    Creates middlewares to handle things

---
+++ middlewares/login.js
@@ -0,0 +1,29 @@
+const enums = require('../enums');
+
+const checkPassword = (req, res, next) => {
+  const { password } = req.body;
+  if (!password || password === '') {
+    return res.status(400).json(enums.password.isNull);
+  } else if (password.length < 6) {
+    return res.status(400).json(enums.password.isSmall);
+  } else {
+    next();
+  }
+};
+
+const checkEmail = (req, res, next) => {
+  const { email } = req.body;
+  const pattern = new RegExp(/[A-Z0-9]{1,}@[A-Z0-9]{2,}\.[A-Z0-9]{2,}/, 'i');
+  if (!email || email === '') {
+    return res.status(400).json(enums.email.isNull);
+  } else if (!pattern.test(email)) {
+    return res.status(400).json(enums.email.isInvalid);
+  } else {
+    next();
+  }
+};
+
+module.exports = {
+  checkPassword,
+  checkEmail,
+};
