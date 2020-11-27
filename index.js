8436cb6e312561c1d7eaf08ead85f4e6577f3ed3
Preto115 <preto115@gmail.com>
2020-11-27 17:49:33 -0300

    teste

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const crypto = require('crypto');
-
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const rescue = require('express-rescue');
 
 const middlewares = require('./middlewares');
-const crushes = require('./crush.json');
-
-const length = crushes.length;
+// const = readFile
+// const length = crushes.length;
 
 const app = express();
 app.use(bodyParser.json());
@@ -22,24 +21,30 @@ app.post('/login', middlewares.login, rescue(async (req, res) => {
   res.status(200).json({ token });
 }));
 
-app.post('/crush', middlewares.tokenValidation, middlewares.createCrush, rescue(async (req, res) => {
-  const { name, age, date } = req.body;
-  const id = crushes.length + 1;
-  const newCrush = { id, name, age, date };
-  crushes.push(newCrush);
+// app.post('/crush', middlewares.tokenValidation, middlewares.createCrush, rescue(async (req, res) => {
+//   const readFromFile = await fs.readFile('./crush.json', 'utf8', (_err, data) => data);
+//   const array = JSON.parse(readFromFile);
 
-  console.log(newCrush);
-  return res.status(201).json(newCrush);
-}));
+//   console.log(array);
+//   const { name, age, date } = req.body;
+//   const id = array.length + 1;
+//   const newCrush = { id, name, age, date };
+//   fs.writeFileSync(readFromFile, array);
+//   array.push(newCrush);
 
-app.get('/crush', middlewares.tokenValidation, (_req, res) => {
-  const cautchUp = crushes.some(create => create.id > length);
+//   console.log(newCrush);
+//   return res.status(201).json(newCrush);
+// }));
 
-  if (length === crushes.length) {
-    return res.status(200).json([]);
-  }
-  if (cautchUp.length > 0) return res.status(200).json(cautchUp);
-  res.status(200).json(crushes);
+app.get('/crush', middlewares.tokenValidation, async (_req, res) => {
+  const readFromFile = await fs.readFile('crush.json');
+  console.log(readFromFile);
+  const array = JSON.parse(readFromFile);
+  // if (length === crushes.length) {
+  //   return res.status(200).json([]);
+  // }
+  // if (cautchUp.length > 0) return res.status(200).json(cautchUp);
+  res.status(200).json(array);
 });
 
 app.use((err, _req, res, _next) => {
