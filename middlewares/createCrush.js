dfa11edcb90b537bea0e9a2426c2b089fbbd63f5
marinabrcls <barcelos.marina@gmail.com>
2020-11-27 21:23:33 -0300

    achei você, errinho

--- middlewares/createCrush.js
+++ middlewares/createCrush.js
@@ -1,10 +1,10 @@
-const { readCrush, addCrush } = require('../services/crud');
+const { readCrush, createCrush } = require('../services/crud');
 
 module.exports = async (req, res) => {
   const { name, age, date } = req.body;
   const newArrayOfCrushes = await readCrush();
   // increment id
   const id = newArrayOfCrushes.length + 1;
-  addCrush(newArrayOfCrushes, { id, name, age, date });
+  createCrush(newArrayOfCrushes, { id, name, age, date });
   res.status(201).json({ id, name, age, date });
 };
