b50e4d5ebe35b0f7a02cb4e551ee4fb30da0d10b
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-12-03 06:18:10 -0300

    requisito 6 passou!

---
+++ middleWares/deletedCrush.js
@@ -0,0 +1,25 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const readCrushs = async () => {
+  const crushs = await fs.readFile('crush.json', 'utf-8', (err) => {
+    if (err) throw console.log('não dá mais');
+  });
+  return JSON.parse(crushs);
+};
+
+const writeCrushs = async (crush) => {
+  fs.writeFile(path.resolve(__dirname, '.', 'crush.json'), JSON.stringify(crush), (err) => {
+    if (err) throw console.log(`Deleta se não eu choro ${err}`);
+  });
+};
+
+module.exports = async (req, res) => {
+  const results = await readCrushs();
+  const id = Number(req.params.id);
+  const filteredCrush = results.filter((crush) => crush.id !== id);
+
+  await writeCrushs('./crush.json', filteredCrush);
+
+  return res.status(200).json({ message: 'Crush deletado com sucesso' });
+};
