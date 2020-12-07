2c3c63da94a4d07cc4f994684f2d46b7081420f1
procopio420 <lucas-procopio@outlook.com>
2020-12-07 00:47:09 -0300

    feat: new model

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const { auth, crush } = require('./controllers');
 
 const app = express();
+app.use(bodyParser.json());
+
+app.use('/login', auth);
+app.use('/crush', crush);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.listen(3000, console.log('IH ALÁ, OLHA SÓ QUEM STARTOU'));
