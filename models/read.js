82ad951826f50919aeebbcc63dd3295598a86239
procopio420 <lucas-procopio@outlook.com>
2020-12-06 23:20:55 -0300

    fix: Number parse to id

--- models/read.js
+++ models/read.js
@@ -3,5 +3,5 @@ const getCrushFile = require('./file/get');
 module.exports = (crushIdToRead = null) => getCrushFile().then((crushFile) => {
   if (!crushIdToRead) return JSON.parse(crushFile);
 
-  return JSON.parse(crushFile).find((crush) => crush.id === crushIdToRead);
+  return JSON.parse(crushFile).find((crush) => Number(crush.id) === Number(crushIdToRead));
 });
