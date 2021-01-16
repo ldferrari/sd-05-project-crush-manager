f7b928a93956d13bf267af0582cffcee4649ddb0
Larissa Palombo <larissampalombo@gmail.com>
2021-01-16 17:26:44 -0300

    inicio de novo

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const middlewares = require('./middlewares');
 
 const app = express();
+app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.post('/login', middlewares.login);
+// app.get('/crush', middlewares.token, middlewares.getCrush);
+// app.post('/crush', middleares.token, middlewares.createCrush);
+// app.get('/crush/:id', middlewares.token, middlewares.searchCrush);
+// app.put('/crush/:id', middlewares.token, middlewares.editCrush);
+// app.delete('/crush/:id', middlewares.token, middlewares.deleteCrush);
+
+const PORT = 3000;
+app.listen(PORT, console.log(`Toc toc, é o crush? ${PORT}`));
---
+++ middlewares/auth.js
@@ -0,0 +1,12 @@
+module.exports = async (req, res, next) => {
+  const { auth } = req.headers;
+
+  if (!auth) {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+  if (auth.length !== 16) {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+
+  return next();
+};
