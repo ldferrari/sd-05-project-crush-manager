5e772eaa3bbbab8a7ba7e1901807b7ff251c0990
Larissa Palombo <larissampalombo@gmail.com>
2021-01-16 17:45:55 -0300

    eslint

--- middlewares/login.js
+++ middlewares/login.js
@@ -5,13 +5,13 @@ module.exports = async (req, res) => {
   const { email, password } = req.body;
 
   // regex https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
-  const validateEmail = (email) => {
+  const validateEmail = (myEmail) => {
     const reg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return reg.test(String(email).toLowerCase());
+    return reg.test(String(myEmail).toLowerCase());
   };
 
-  const validadePassword = (password) => {
-    if (password.length >= 6) {
+  const validadePassword = (myPassword) => {
+    if (myPassword.length >= 6) {
       return true;
     }
     return false;
@@ -20,13 +20,13 @@ module.exports = async (req, res) => {
   if (!email) {
     return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
-  if (!validateEmail(email)) {
+  if (!validateEmail(myEmail)) {
     return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
   if (!password) {
     return res.status(400).json({ message: 'O campo "password" é obrigatório' });
   }
-  if (!validadePassword(password)) {
+  if (!validadePassword(myPassword)) {
     return res.status(400).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
   }
   return res.status(200).json({ token });
