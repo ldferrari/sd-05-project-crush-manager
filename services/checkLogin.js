3b701ec776dbf37d6f65e4e3ea0bd26679e51ff0
samueap <samueap@gmail.com>
2021-01-16 17:36:12 -0300

    fix lint

--- services/checkLogin.js
+++ services/checkLogin.js
@@ -1,6 +1,6 @@
 // Validação do email por regex no formado "email@email.com"
 function checkEmail(email) {
-  const validRegex = new Regexp (/^.+@.+.(.{3}|.{2})$/);
+  const validRegex = (/^.+@.+.(.{3}|.{2})$/);
   // const validRegex = /[a-z0-9\._%+!$&*=^|~#%'`?{}/\-]+@([a-z0-9\-]+\.){1,}([a-z]{2,16})/;
   return validRegex.test((email).toLowerCase());
 } // Retorna true or false
