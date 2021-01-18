fefd79fc0ba7b80eb5e3784d67e73e937e35a65e
samueap <samueap@gmail.com>
2021-01-18 18:05:34 -0300

    lint

--- middlewares/updateCrush.js
+++ middlewares/updateCrush.js
@@ -1,9 +1,9 @@
-const fs = require('fs').promises
+const fs = require('fs').promises;
 const { readJSON } = require('../services/allCrush');
 
 module.exports = async (req, res) => {
   const { name, age, date } = req.body;
-  const id = parseInt(req.params.id);
+  const id = parseInt(req.params.id, 10);
   const newArry = await readJSON();
   const findCrush = newArry.find((crush) => crush.id === id);
   newArry[newArry.indexOf(findCrush)] = { id, name, age, date };
